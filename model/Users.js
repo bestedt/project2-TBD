a2dec2c90345bb2b9b4469e31e14c2b58af69dd5
AnthoG <anthony.dswg@gmail.com>
2023-11-13 21:25:16 -0500

    Model User

---
+++ model/Users.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_type:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [['tenant', 'superintendent', 'aptmanager']]
+        }
+
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
