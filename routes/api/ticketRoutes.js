979f45daae2dbcc19c57a034b666de545670a9c7
jizhang80 <zhangji80@outlook.com>
2023-11-14 19:17:43 -0500

    add ticket routes

---
+++ routes/api/ticketRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Ticket } = require('../../models');
+
+// TODO: get all the ticket
+
+// TODO: get ticket by id
+
+// TODO: get ticket by status
+
+// TODO: post, creat new ticket
+
+// TODO: put, update ticket status
