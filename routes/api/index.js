9e04311248f3fc661ad78e3d1287866b1525890d
jizhang80 <zhangji80@outlook.com>
2023-11-18 20:02:30 -0500

    change main view

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const ticketRoutes = require('./ticketRoutes');
 const userRoutes = require('./userRoutes');
-const signInRoutes = require('./signInRoutes');
+
 router.use('/tickets', ticketRoutes);
 router.use('/users', userRoutes);
-router.use('/login', signInRoutes);
 module.exports = router;
