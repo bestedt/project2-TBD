475e99dde0eaec47a1bff7608ca703852db582d6
jizhang80 <zhangji80@outlook.com>
2023-11-20 15:16:56 -0500

    add data chart for manager

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const ticketRoutes = require('./ticketRoutes');
 const userRoutes = require('./userRoutes');
+const dataRoutes = require('./dataRoutes');
 
+router.use('/data', dataRoutes);
 router.use('/tickets', ticketRoutes);
 router.use('/users', userRoutes);
 module.exports = router;
